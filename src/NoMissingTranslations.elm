1edad1f1771ae81a8f9281ac270bea9ff91d4846
Dunja Lalic <dunja.lalic@gmail.com>
2021-08-06 21:33:32 +0200

    Add simple implementation

--- src/NoMissingTranslations.elm
+++ src/NoMissingTranslations.elm
@@ -6,7 +6,17 @@ module NoMissingTranslations exposing (rule)
 
 -}
 
-import Review.Rule as Rule exposing (Rule)
+import Elm.Syntax.Exposing as Exposing
+import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Import exposing (Import)
+import Elm.Syntax.ModuleName exposing (ModuleName)
+import Elm.Syntax.Node as Node exposing (Node)
+import Review.Rule as Rule exposing (Error, Rule)
+
+
+type Context
+    = NoImport
+    | Import { aliasing : Maybe ModuleName, exposed : Bool }
 
 
 {-| Reports... REPLACEME
@@ -45,6 +55,67 @@ elm-review --template dlalic/elm-review-i18n/example --rules NoMissingTranslatio
 -}
 rule : Rule
 rule =
-    Rule.newModuleRuleSchema "NoMissingTranslations" ()
-        -- Add your visitors
+    Rule.newModuleRuleSchema "NoMissingTranslations" NoImport
+        |> Rule.withImportVisitor importVisitor
+        |> Rule.withExpressionVisitor expressionVisitor
         |> Rule.fromModuleRuleSchema
+
+
+importVisitor : Node Import -> Context -> ( List (Error {}), Context )
+importVisitor node context =
+    case Node.value node |> .moduleName |> Node.value of
+        [ "Html" ] ->
+            ( []
+            , let
+                exposed =
+                    Node.value node
+                        |> .exposingList
+                        |> Maybe.map (Node.value >> Exposing.exposesFunction "text")
+                        |> Maybe.withDefault False
+
+                aliasing =
+                    Node.value node |> .moduleAlias |> Maybe.map Node.value
+              in
+              Import { aliasing = aliasing, exposed = exposed }
+            )
+
+        _ ->
+            ( [], context )
+
+
+expressionVisitor : Node Expression -> Rule.Direction -> Context -> ( List (Error {}), Context )
+expressionVisitor expression direction context =
+    case ( direction, context ) of
+        ( Rule.OnEnter, Import { aliasing, exposed } ) ->
+            case Node.value expression of
+                Expression.Application [ function, argument ] ->
+                    case ( Node.value function, Node.value argument ) of
+                        ( Expression.FunctionOrValue [] "text", Expression.Literal str ) ->
+                            if exposed then
+                                ( [ Rule.error (error str) (Node.range expression) ], context )
+
+                            else
+                                ( [], context )
+
+                        ( Expression.FunctionOrValue moduleName "text", Expression.Literal str ) ->
+                            if moduleName == (aliasing |> Maybe.withDefault [ "Html" ]) then
+                                ( [ Rule.error (error str) (Node.range expression) ], context )
+
+                            else
+                                ( [], context )
+
+                        _ ->
+                            ( [], context )
+
+                _ ->
+                    ( [], context )
+
+        _ ->
+            ( [], context )
+
+
+error : String -> { message : String, details : List String }
+error text =
+    { message = "Found hardcoded user facing text: " ++ text
+    , details = [ "Localize it with elm-i18next-gen" ]
+    }
--- tests/NoMissingTranslationsTest.elm
+++ tests/NoMissingTranslationsTest.elm
@@ -8,17 +8,23 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoMissingTranslations"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report an error when Html.text is hardcoded" <|
             \() ->
-                """module A exposing (..)
-a = 1
+                """module NoMissingTranslations exposing (..)
+import Html exposing (Html, h1, text)
+
+view : View msg
+view =
+    [ h1 [] [ text (hello translations name) ]
+    , h2 [] [ text "This text is user facing" ]
+    ]
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            { message = "Found hardcoded user facing text: This text is user facing"
+                            , details = [ "Localize it with elm-i18next-gen" ]
+                            , under = "text \"This text is user facing\""
                             }
                         ]
         ]
