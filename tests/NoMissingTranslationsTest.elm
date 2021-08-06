d19fe2b5d5fc49ce4e536b39503aa8eaf2ebb6be
Dunja Lalic <dunja.lalic@gmail.com>
2021-08-06 23:36:34 +0200

    Make tests more realistic

--- tests/NoMissingTranslationsTest.elm
+++ tests/NoMissingTranslationsTest.elm
@@ -11,20 +11,21 @@ all =
         [ test "should report an error when Html.text is hardcoded" <|
             \() ->
                 """module NoMissingTranslations exposing (..)
-import Html exposing (Html, h1, text)
+import Html exposing (h1, h2, text)
+import I18Next
 
-view : View msg
-view =
+view : I18Next.Translations -> String -> Html msg
+view translations name =
     [ h1 [] [ text (hello translations name) ]
-    , h2 [] [ text "This text is user facing" ]
+    , h2 [] [ text "This text is hardcoded" ]
     ]
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "Found hardcoded user facing text: This text is user facing"
+                            { message = "Found hardcoded user facing text: This text is hardcoded"
                             , details = [ "" ]
-                            , under = "text \"This text is user facing\""
+                            , under = "text \"This text is hardcoded\""
                             }
                         ]
         , test "should not report an error text is not from Html module" <|
@@ -32,8 +33,8 @@ view =
                 """module NoMissingTranslations exposing (..)
 import Foo exposing (text)
 
-init : Cmd msg
-init =
+foo : Html msg
+foo =
     text ""
 """
                     |> Review.Test.run rule
